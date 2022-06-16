e044661ed63b103bb4f3a173ffd1b477a82c85c2
ftm-ramezankhani <fatemeh.rr281@gmail.com>
2022-06-16 09:08:39 +0430

    seccondcommit

--- app/src/main/java/ir/shariaty/power/MainActivity2.java
+++ app/src/main/java/ir/shariaty/power/MainActivity2.java
@@ -10,5 +10,29 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        package ir.shariaty.power;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+        public class MainActivity2 extends AppCompatActivity {
+
+            @Override
+            protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_main2);
+                ImageView img2=(ImageView) findViewById(R.id.imglamp2);
+                img2.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        startActivity(new Intent((MainActivity2.this),MainActivity.class));
+                    }
+                });
+            }
+        }
     }
 }
